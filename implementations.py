c6a2324212ada65845acfe184af64e9f11edc58f
yf0726 <fuyan_1996@outlook.com>
2018-10-20 14:56:22 +0200

    update

--- implementations.py
+++ implementations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-
+import csv
 # -----------   helpers            ---------- #
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
@@ -27,16 +27,35 @@ def standardize(x):
     x_standardize = np.empty_like(x, dtype='float64')
 
     mean_x = np.mean(x, axis=0) #x 为一个ndarray或array-like对象
-    for row in range(x.shape[1]):
-        x_standardize[row] = x[row] - mean_x[row]
+    for colomn in range(x.shape[1]):
+        x_standardize[:, colomn] = x[:, colomn] - mean_x[colomn]
 
     std_x = np.std(x, axis=0)
-    for row in range(x.shape[1]):
-        x_standardize[row] = x_standardize[row] / std_x[row]
+    for colomn in range(x.shape[1]):
+        x_standardize[:, colomn] = x_standardize[:, colomn] / std_x[colomn]
 
     return x_standardize, mean_x, std_x
+def predict_labels(weights, data):
+    """Generates class predictions given weights, and a test data matrix"""
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0)] = -1
+    y_pred[np.where(y_pred > 0)] = 1
+    
+    return y_pred
 
-
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
 
 # ----------    Cost calculation    ---------- #
 def calculate_mse(e):
